f3e7621500a938d2fba8cc67eb088075b8e1ad46
Manish0085 <manishk57107@gmail.com>
2026-01-25 18:33:35 +0530

    Adding other controller

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,137 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+/**
+ * CREATE TWEET
+ */
+const createTweet = asyncHandler(async (req, res) => {
+    const { content } = req.body
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Tweet content is required")
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user._id
+    })
+
+    res.status(201).json(
+        new ApiResponse(201, tweet, "Tweet created successfully")
+    )
+})
+
+/**
+ * GET ALL TWEETS OF A USER
+ */
+const getUserTweets = asyncHandler(async (req, res) => {
+    const { userId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user ID")
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        { $sort: { createdAt: -1 } },
+        { $skip: (page - 1) * limit },
+        { $limit: Number(limit) },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        { $unwind: "$owner" }
+    ])
+
+    res.status(200).json(
+        new ApiResponse(200, tweets, "User tweets fetched successfully")
+    )
+})
+
+/**
+ * UPDATE TWEET
+ */
+const updateTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+    const { content } = req.body
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Updated content is required")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to update this tweet")
+    }
+
+    tweet.content = content
+    await tweet.save()
+
+    res.status(200).json(
+        new ApiResponse(200, tweet, "Tweet updated successfully")
+    )
+})
+
+/**
+ * DELETE TWEET
+ */
+const deleteTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to delete this tweet")
+    }
+
+    await tweet.deleteOne()
+
+    res.status(200).json(
+        new ApiResponse(200, {}, "Tweet deleted successfully")
+    )
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
