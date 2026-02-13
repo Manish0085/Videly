a6dce3377c437c3244de670770264709ced03e2d
Manish0085 <manishk57107@gmail.com>
2026-02-13 19:06:18 +0530

    Commit Changes

---
+++ backend-java/src/main/java/com/manish/videostreaming/controller/GeneralController.java
@@ -0,0 +1,14 @@
+package com.manish.videostreaming.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GeneralController {
+
+    @GetMapping
+    public String testApp(){
+        return "Everything is going well";
+    }
+}
